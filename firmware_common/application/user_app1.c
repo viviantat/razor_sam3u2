67c3a5e9aebf3bb6d37651f59010b461722b1a2a
viviantat <vivianvtat@gmail.com>
2024-09-26 20:15:02 -0600

    testing a commit

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -3,6 +3,7 @@
 @brief User's tasks / applications are written here.  This description
 should be replaced by something specific to the task.
 
+testing adding this line and deleting below
 ----------------------------------------------------------------------------------------------------------------------
 To start a new task using this user_app1 as a template:
  1. Copy both user_app1.c and user_app1.h to the Application directory
